b33ea8994be779c0505e319c7f7cf49d84eb7015
SANA BEN <sana.bengannoune@gmail.com>
2020-04-22 15:21:38 +0000

    Récolte des réponses dans un Objet

---
+++ app.js
@@ -0,0 +1,90 @@
+let questions = [
+    {
+    "question":"Pensez-vous avoir ou avoir eu de la fièvre ces 10 derniers jours (frissons, sueurs) ?",
+    "option1":"Oui",
+    "option2":"Non"
+}, {
+    "question":"Quelle est votre température corporelle ?",
+    "option1":"",
+}, {
+    "question":"Ces derniers jours, avez-vous une toux ou une augmentation de votre toux habituelle ?",
+    "option1":"Oui",
+    "option2":"Non"
+}, {
+    "question":"Avez-vous eu des courbatures inhabituelles au cours des derniers jours ?",
+    "option1":"Oui",
+    "option2":"Non"
+}, {
+    "question":"Ces derniers jours, avez-vous un mal de gorge ?",
+    "option1":"Oui",
+    "option2":"Non"
+}, {
+    "question":"Ces dernières 24 heures, avez-vous de la diarrhée ? Avec au moins 3 selles molles .",
+    "option1":"Oui",
+    "option2":"Non"
+}, {
+    "question":"Ces derniers jours, avez-vous une fatigue inhabituelle qui vous a obligé à vous reposer plus de la moitié de la journée ?",
+    "option1":"Oui",
+    "option2":"Non"
+}, {
+    "question":"Avez-vous des difficultés importantes pour vous alimenter ou boire depuis plus de 24h ?",
+    "option1":"Oui",
+    "option2":"Non"
+}, {
+    "question":"Dans les dernières 24 heures, avez-vous noté un manque de souffle inhabituel lorsque vous parlez ou faites un petit effort ?",
+    "option1":"Oui",
+    "option2":"Non"
+}, {
+    "question":"Actuellement, comment vous vous sentez ?",
+    "option1":"Bien",
+    "option2":"Assez bien",
+    "option3":"Fatigué(e)",
+    "option4":"Très fatigué(e)"
+}, {
+    "question":"Quel est votre âge ? Ceci, afin de calculer un facteur de risque spécifique .",
+    "option1":""
+}, {
+    "question":"Quel est votre poids ? Afin de calculer l’indice de masse corporelle qui est un facteur influençant le risque de complications de l’infection.",
+    "option1":""
+}, {
+    "question":"Quelle est votre taille ? Afin de calculer l’indice de masse corporelle qui est un facteur influençant le risque de complications de l’infection.",
+    "option1":""
+}, {
+    "question":"Avez-vous de l’hypertension artérielle mal équilibrée ? Ou avez-vous une maladie cardiaque ou vasculaire ? Ou prenez-vous un traitement à visée cardiologique ?",
+    "option1":"Oui",
+    "option2":"Non"
+}, {
+    "question":"Êtes-vous diabétique ?",
+    "option1":"Oui",
+    "option2":"Non"
+}, {
+    "question":"Avez-vous ou avez-vous eu un cancer ?",
+    "option1":"Oui",
+    "option2":"Non"
+}, {
+    "question":"Avez-vous une maladie respiratoire ? Ou êtes-vous suivi par un pneumologue ?",
+    "option1":"Oui",
+    "option2":"Non"
+},{
+    "question":"Avez-vous une insuffisance rénale chronique dialysée ?",
+    "option1":"Oui",
+    "option2":"Non"
+}, {
+    "question":"Avez-vous une maladie chronique du foie ?",
+    "option1":"Oui",
+    "option2":"Non"
+}, {
+    "question":"Êtes-vous enceinte ?",
+    "option1":"Oui",
+    "option2":"Non",
+    "option3":"Homme"
+}, {
+    "question":"Avez-vous une maladie connue pour diminuer vos défenses immunitaires ?",
+    "option1":"Oui",
+    "option2":"Non",
+}, {
+    "question":"Prenez-vous un traitement immunosuppresseur ? C’est un traitement qui diminue vos défenses contre les infections. Voici quelques exemples : corticoïdes, méthotrexate, ciclosporine, tacrolimus, azathioprine, cyclophosphamide (liste non exhaustive).",
+    "option1":"Oui",
+    "option2":"Non",
+}   
+]
