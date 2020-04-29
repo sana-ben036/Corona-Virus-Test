a0aa29cf952cb8576f9d16b675ec90d2526a23c1
SANA BEN <sana.bengannoune@gmail.com>
2020-04-29 03:46:04 +0000

    modification structure of array

--- app.js
+++ app.js
@@ -1,93 +1,81 @@
 // ::: array of quiz data
 
+
 let questions = [
     {
-    "question":"Pensez-vous avoir ou avoir eu de la fièvre ces 10 derniers jours (frissons, sueurs) ?",
-    "option1":"Oui",
-    "option2":"Non"
+    question:"Pensez-vous avoir ou avoir eu de la fièvre ces 10 derniers jours (frissons, sueurs) ?",
+    options:'<label for="choix1" class="label-radio"><input id="choix1" type="radio" name="option" value="oui"><i class="fas fa-check"></i></input><span>Oui</span></label><br><label for="choix2" class="label-radio" ><input id="choix2" type="radio" name="option" value="non"><i class="fas fa-times"></i></input><span>Non</span></label>'
+    
 }, {
-    "question":"Quelle est votre température corporelle ?",
-    "option1":"",
+    question:"Quelle est votre température corporelle ?",
+    options:'<label for="choix" class="label-number"><input id="choix" type="number" name="option" min="34" max="42" placeholder="34-42"></input> degrés </label>'
 }, {
-    "question":"Ces derniers jours, avez-vous une toux ou une augmentation de votre toux habituelle ?",
-    "option1":"Oui",
-    "option2":"Non"
+    question:"Ces derniers jours, avez-vous une toux ou une augmentation de votre toux habituelle ?",
+    options:'<label for="choix1" class="label-radio"><input id="choix1" type="radio" name="option" value="oui"><i class="fas fa-check"></i></input><span>Oui</span></label><br><label for="choix2" class="label-radio" ><input id="choix2" type="radio" name="option" value="non"><i class="fas fa-times"></i></input><span>Non</span></label>'
 }, {
-    "question":"Avez-vous eu des courbatures inhabituelles au cours des derniers jours ?",
-    "option1":"Oui",
-    "option2":"Non"
+    question:"Avez-vous eu des courbatures inhabituelles au cours des derniers jours ?",
+    options:'<label for="choix1" class="label-radio"><input id="choix1" type="radio" name="option" value="oui"><i class="fas fa-check"></i></input><span>Oui</span></label><br><label for="choix2" class="label-radio" ><input id="choix2" type="radio" name="option" value="non"><i class="fas fa-times"></i></input><span>Non</span></label>'
 }, {
-    "question":"Ces derniers jours, avez-vous un mal de gorge ?",
-    "option1":"Oui",
-    "option2":"Non"
+    question:"Ces derniers jours, avez-vous un mal de gorge ?",
+    options:'<label for="choix1" class="label-radio"><input id="choix1" type="radio" name="option" value="oui"><i class="fas fa-check"></i></input><span>Oui</span></label><br><label for="choix2" class="label-radio" ><input id="choix2" type="radio" name="option" value="non"><i class="fas fa-times"></i></input><span>Non</span></label>'
 }, {
-    "question":"Ces dernières 24 heures, avez-vous de la diarrhée ? Avec au moins 3 selles molles .",
-    "option1":"Oui",
-    "option2":"Non"
+    question:"Ces dernières 24 heures, avez-vous de la diarrhée ? Avec au moins 3 selles molles .",
+    options:'<label for="choix1" class="label-radio"><input id="choix1" type="radio" name="option" value="oui"><i class="fas fa-check"></i></input><span>Oui</span></label><br><label for="choix2" class="label-radio" ><input id="choix2" type="radio" name="option" value="non"><i class="fas fa-times"></i></input><span>Non</span></label>'
 }, {
-    "question":"Ces derniers jours, avez-vous une fatigue inhabituelle qui vous a obligé à vous reposer plus de la moitié de la journée ?",
-    "option1":"Oui",
-    "option2":"Non"
+    question:"Ces derniers jours, avez-vous une fatigue inhabituelle qui vous a obligé à vous reposer plus de la moitié de la journée ?",
+    options:'<label for="choix1" class="label-radio"><input id="choix1" type="radio" name="option" value="oui"><i class="fas fa-check"></i></input><span>Oui</span></label><br><label for="choix2" class="label-radio" ><input id="choix2" type="radio" name="option" value="non"><i class="fas fa-times"></i></input><span>Non</span></label>'
 }, {
-    "question":"Avez-vous des difficultés importantes pour vous alimenter ou boire depuis plus de 24h ?",
-    "option1":"Oui",
-    "option2":"Non"
+    question:"Avez-vous des difficultés importantes pour vous alimenter ou boire depuis plus de 24h ?",
+    options:'<label for="choix1" class="label-radio"><input id="choix1" type="radio" name="option" value="oui"><i class="fas fa-check"></i></input><span>Oui</span></label><br><label for="choix2" class="label-radio" ><input id="choix2" type="radio" name="option" value="non"><i class="fas fa-times"></i></input><span>Non</span></label>'
 }, {
-    "question":"Dans les dernières 24 heures, avez-vous noté un manque de souffle inhabituel lorsque vous parlez ou faites un petit effort ?",
-    "option1":"Oui",
-    "option2":"Non"
+    question:"Dans les dernières 24 heures, avez-vous noté un manque de souffle inhabituel lorsque vous parlez ou faites un petit effort ?",
+    options:'<label for="choix1" class="label-radio"><input id="choix1" type="radio" name="option" value="oui"><i class="fas fa-check"></i></input><span>Oui</span></label><br><label for="choix2" class="label-radio" ><input id="choix2" type="radio" name="option" value="non"><i class="fas fa-times"></i></input><span>Non</span></label>'
 }, {
-    "question":"Actuellement, comment vous vous sentez ?",
-    "option1":"Bien",
-    "option2":"Assez bien",
-    "option3":"Fatigué(e)",
-    "option4":"Très fatigué(e)"
+    question:"Actuellement, comment vous vous sentez ?",
+    options:'<label for="choix1" class="label-radio"><input id="choix1" type="radio" name="option" value="Bien"><i class="fas fa-check"></i></input><span>Bien</span></label><br><label for="choix2" class="label-radio" ><input id="choix2" type="radio" name="option" value="Assez bien"><i class="fas fa-times"></i></input><span>Assez bien</span></label><br><label for="choix3" class="label-radio"><input id="choix3" type="radio" name="option" value="Fatigué(e)"><i class="fas fa-check"></i></input><span>Fatigué(e)</span></label><br><label for="choix4" class="label-radio" ><input id="choix4" type="radio" name="option" value="Très fatigué(e)"><i class="fas fa-times"></i></input><span>Très fatigué(e)</span></label>' 
 }, {
     "question":"Quel est votre âge ? Ceci, afin de calculer un facteur de risque spécifique .",
-    "option1":""
+    "option1":"Age"
 }, {
     "question":"Quel est votre poids ? Afin de calculer l’indice de masse corporelle qui est un facteur influençant le risque de complications de l’infection.",
-    "option1":""
+    "option1":"Poid"
 }, {
     "question":"Quelle est votre taille ? Afin de calculer l’indice de masse corporelle qui est un facteur influençant le risque de complications de l’infection.",
-    "option1":""
+    "option1":"Taille"
 }, {
-    "question":"Avez-vous de l’hypertension artérielle mal équilibrée ? Ou avez-vous une maladie cardiaque ou vasculaire ? Ou prenez-vous un traitement à visée cardiologique ?",
-    "option1":"Oui",
-    "option2":"Non"
+    question:"Avez-vous de l’hypertension artérielle mal équilibrée ? Ou avez-vous une maladie cardiaque ou vasculaire ? Ou prenez-vous un traitement à visée cardiologique ?",
+    options:'<label for="choix1" class="label-radio"><input id="choix1" type="radio" name="option" value="oui"><i class="fas fa-check"></i></input><span>Oui</span></label><br><label for="choix2" class="label-radio" ><input id="choix2" type="radio" name="option" value="non"><i class="fas fa-times"></i></input><span>Non</span></label>'
+
 }, {
-    "question":"Êtes-vous diabétique ?",
-    "option1":"Oui",
-    "option2":"Non"
+    question:"Êtes-vous diabétique ?",
+    options:'<label for="choix1" class="label-radio"><input id="choix1" type="radio" name="option" value="oui"><i class="fas fa-check"></i></input><span>Oui</span></label><br><label for="choix2" class="label-radio" ><input id="choix2" type="radio" name="option" value="non"><i class="fas fa-times"></i></input><span>Non</span></label>'
+
 }, {
-    "question":"Avez-vous ou avez-vous eu un cancer ?",
-    "option1":"Oui",
-    "option2":"Non"
+    question:"Avez-vous ou avez-vous eu un cancer ?",
+    options:'<label for="choix1" class="label-radio"><input id="choix1" type="radio" name="option" value="oui"><i class="fas fa-check"></i></input><span>Oui</span></label><br><label for="choix2" class="label-radio" ><input id="choix2" type="radio" name="option" value="non"><i class="fas fa-times"></i></input><span>Non</span></label>'
+
 }, {
-    "question":"Avez-vous une maladie respiratoire ? Ou êtes-vous suivi par un pneumologue ?",
-    "option1":"Oui",
-    "option2":"Non"
+    question:"Avez-vous une maladie respiratoire ? Ou êtes-vous suivi par un pneumologue ?",
+    options:'<label for="choix1" class="label-radio"><input id="choix1" type="radio" name="option" value="oui"><i class="fas fa-check"></i></input><span>Oui</span></label><br><label for="choix2" class="label-radio" ><input id="choix2" type="radio" name="option" value="non"><i class="fas fa-times"></i></input><span>Non</span></label>'
+
 },{
-    "question":"Avez-vous une insuffisance rénale chronique dialysée ?",
-    "option1":"Oui",
-    "option2":"Non"
+    question:"Avez-vous une insuffisance rénale chronique dialysée ?",
+    options:'<label for="choix1" class="label-radio"><input id="choix1" type="radio" name="option" value="oui"><i class="fas fa-check"></i></input><span>Oui</span></label><br><label for="choix2" class="label-radio" ><input id="choix2" type="radio" name="option" value="non"><i class="fas fa-times"></i></input><span>Non</span></label>'
+
 }, {
-    "question":"Avez-vous une maladie chronique du foie ?",
-    "option1":"Oui",
-    "option2":"Non"
+    question:"Avez-vous une maladie chronique du foie ?",
+    options:'<label for="choix1" class="label-radio"><input id="choix1" type="radio" name="option" value="oui"><i class="fas fa-check"></i></input><span>Oui</span></label><br><label for="choix2" class="label-radio" ><input id="choix2" type="radio" name="option" value="non"><i class="fas fa-times"></i></input><span>Non</span></label>'
+
 }, {
-    "question":"Êtes-vous enceinte ?",
-    "option1":"Oui",
-    "option2":"Non",
-    "option3":"Homme"
+    question:"Êtes-vous enceinte ?",
+    options:'<label for="choix1" class="label-radio"><input id="choix1" type="radio" name="option" value="oui"><i class="fas fa-check"></i></input><span>Oui</span></label><br><label for="choix2" class="label-radio" ><input id="choix2" type="radio" name="option" value="non"><i class="fas fa-times"></i></input><span>Non</span></label><br><label for="choix3" class="label-radio"><input id="choix3" type="radio" name="option" value="homme"><i class="fas fa-check"></i></input><span>Homme</span></label>'
 }, {
-    "question":"Avez-vous une maladie connue pour diminuer vos défenses immunitaires ?",
-    "option1":"Oui",
-    "option2":"Non",
+    question:"Avez-vous une maladie connue pour diminuer vos défenses immunitaires ?",
+    options:'<label for="choix1" class="label-radio"><input id="choix1" type="radio" name="option" value="oui"><i class="fas fa-check"></i></input><span>Oui</span></label><br><label for="choix2" class="label-radio" ><input id="choix2" type="radio" name="option" value="non"><i class="fas fa-times"></i></input><span>Non</span></label>'
+
 }, {
-    "question":"Prenez-vous un traitement immunosuppresseur ? C’est un traitement qui diminue vos défenses contre les infections. Voici quelques exemples : corticoïdes, méthotrexate, ciclosporine, tacrolimus, azathioprine, cyclophosphamide (liste non exhaustive).",
-    "option1":"Oui",
-    "option2":"Non",
+    question:"Prenez-vous un traitement immunosuppresseur ? C’est un traitement qui diminue vos défenses contre les infections. Voici quelques exemples : corticoïdes, méthotrexate, ciclosporine, tacrolimus, azathioprine, cyclophosphamide (liste non exhaustive).",
+    options:'<label for="choix1" class="label-radio"><input id="choix1" type="radio" name="option" value="oui"><i class="fas fa-check"></i></input><span>Oui</span></label><br><label for="choix2" class="label-radio" ><input id="choix2" type="radio" name="option" value="non"><i class="fas fa-times"></i></input><span>Non</span></label>'
 }   
 ]
 
@@ -134,15 +122,12 @@ var count = document.getElementById('count');
 var progress = document.getElementById('progress');
 
 
-
-
 // ::: afficher questions
 
 var currentQuestion = 0;
 var totalQuestion = questions.length;
-var para = document.getElementById('para');
-var opt1 = document.getElementById('opt1');
-var opt2 = document.getElementById('opt2');
+var question = document.getElementById('question');
+var options = document.getElementById('options');
 let next= document.getElementById('btn-next');
 let last= document.getElementById('btn-last');
 
@@ -152,20 +137,16 @@ last.addEventListener('click',loadLastQuestion);
 
 function loadQuestion (questionIndex){
     
-    
     var q = questions[questionIndex];
-    para.textContent = q.question;
-    opt1.textContent = q.option1;
-    opt2.textContent = q.option2;
+    question.textContent = q.question;
+    options.innerHTML = q.options;
     count.textContent= (questionIndex + 1) + '/' + totalQuestion;
     progress.style.width = (questionIndex + 1) * 4.55 + '%';
- 
-
 };
 
 function loadNextQuestion(){
 
-    var selectOption = document.querySelector('input[type=radio]:checked');
+    var selectOption = document.querySelector('input:checked');
     if(!selectOption){
         alert('SVP choisir une reponse!');
         return;
